545db20bd45c26445f49d344698f3baa5fd8cfab
Jeremiah Charles <charlesjeremiah89@yahoo.com>
2023-04-17 08:05:22 -0700

    Add strconcat prototype

--- main.h
+++ main.h
@@ -4,4 +4,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+/* STRING FUNCTIONS */
+int _strlen(char *str);
+char *_strcpy(char *dest, const char *src);
+char *_strconcat(char *dest, const char *src);
+char *begins_with(const char *str, const char *substr);
+
 #endif /* MAIN_H */
