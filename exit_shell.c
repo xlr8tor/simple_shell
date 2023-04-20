6eb25f52e5e0f4b2f41c5eaf6c32ac3e878764a3
JamesRaphaelJRC <jamesraphaeljrc@gmail.com>
2023-04-20 02:38:18 -0700

    Created exit functionality

---
+++ exit_shell.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+ * exit_shell - Exits the shell when user types "exit".
+ */
+void exit_shell(void)
+{
+	exit(EXIT_SUCCESS);
+}
---
+++ strcmp.c
@@ -0,0 +1,10 @@
+include "main.h"
+
+/**
+ * strcmp - Compares two strings.
+ * @s1: A pointer to the first string.
+ * @s2: A pointer to the second string.
+ *
+ * Return: 0 if s1 == s2, an integer < 0 if s1 < s2, or integer > 0 if s1 > s2
+ */
+int _strcmp(const char *s1, const char *s2)
